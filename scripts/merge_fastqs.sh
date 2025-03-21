3917aa2f1fe7b4aa08a62d456670cc8af94183df
dvalcarcel <david.valcarcel@salud.madrid.org>
2025-03-21 22:56:38 +0100

    Add files via upload
    Añadiendo los scripts a excepción del #TODO de log

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -3,3 +3,13 @@
 # stored in the output directory specified by the second argument ($2).
 #
 # The directory containing the samples is indicated by the first argument ($1).
+
+#En primer lugar, creamos el directorio de salida si no existe:
+mkdir -p $2
+
+file_name_1=$(ls "$1"/"$3"*.fastq.gz | cut -d "/" -f2 | sort | head -n 1) #Obtenemos el nombre del primer archivo fastq.gz del directorio data que contenga el id de la muestra.
+file_name_2=$(ls "$1"/"$3"*.fastq.gz | cut -d "/" -f2 | sort | tail -n 1) #Obtenemos el nombre del último archivo fastq.gz del directorio data que contenga el id de la muestra.
+
+echo "Merging files $file_name_1 and $file_name_2"
+
+cat "$1/$file_name_1" $1/$file_name_2 > "$2/$3.merged.fastq.gz" #Concatenamos los dos archivos fastq.gz en un solo archivo con el nombre de la muestra y lo guardamos en el directorio de salida.
--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -21,19 +21,31 @@ do
 done
 
 # TODO: run cutadapt for all merged files
-# cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed \
-#     -o <trimmed_file> <input_file> > <log_file>
+
+mkdir -p out/trimmed #Creamos el directorio out/trimmed si no existe.
+mkdir -p log/cutadapt #Creamos el directorio log/cutadapt si no existe.
+for file in out/merged/*.fastq.gz #Hacemos un bucle for para coger las muestras del directorio out/merged.
+do
+    sid=$(basename "$file" .merged.fastq.gz) #Obtenemos el id de la muestra.
+    echo "Running cutadapt for sample $sid"
+    cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed \
+         -o out/trimmed/"$sid.trimmed.fastq.gz" "$file" > log/cutadapt/"$sid.log" 
+         #Ejecutamos cutadapt con los merged.fastq.gz, guardamos el resultado en out/trimmed y redireccionamos el log en el directorio log/cutadapt.
+done
+
 
 # TODO: run STAR for all trimmed files
-#for fname in out/trimmed/*.fastq.gz
-#do
+for fname in out/trimmed/*.fastq.gz #Hacemos un bucle for para coger las muestras del directorio out/trimmed.
+do
     # you will need to obtain the sample ID from the filename
-#    sid=#TODO
-    # mkdir -p out/star/$sid
-    # STAR --runThreadN 4 --genomeDir res/contaminants_idx \
-    #    --outReadsUnmapped Fastx --readFilesIn <input_file> \
-    #    --readFilesCommand gunzip -c --outFileNamePrefix <output_directory>
-#done 
+    sid=$(basename "$fname" .trimmed.fastq.gz) #Obtenemos el id de la muestra.
+    echo "Running STAR Alignment for sample $sid"
+    mkdir -p out/star/$sid
+    STAR --runThreadN 4 --genomeDir res/contaminants_idx/ \
+        --outReadsUnmapped Fastx --readFilesIn $fname \
+        --readFilesCommand gunzip -c --outFileNamePrefix out/star/$sid/
+        #Ejecutamos STAR con los archivos trimmed.fastq.gz y guardamos el resultado en out/star/$sid.
+done 
 
 # TODO: create a log file containing information from cutadapt and star logs
 # (this should be a single log file, and information should be *appended* to it on each run)
